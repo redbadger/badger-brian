86c310de61fd24c008ec49a9aa9fcfccfdbc4ec1
David Thompson <david.arthur.thompson@gmail.com>
2021-03-30 17:24:11 +0100

    add ping endpoint

--- lib/slack/src/index.ts
+++ lib/slack/src/index.ts
@@ -1,1 +1,11 @@
-console.log('howdy')
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/ping', (_req, res) => {
+  res.send('pong')
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))

5ef176d634a3e818ab99d08e4c9a788d75ed94c1
Cedd Burge <ceddlyburge@gmail.com>
2021-04-16 15:26:28 +0100

    Remove dead wood

--- lib/slack/src/index.ts
+++ lib/slack/src/index.ts
@@ -3,14 +3,12 @@ import fetch from 'node-fetch';
 
 const app = express()
 
-// These environment variables are injected automatically into the container by dapr
+// This environment variables are injected automatically into the container by dapr
 const daprPort = process.env.DAPR_HTTP_PORT;
-//const daprGRPCPort = process.env.DAPR_GRPC_PORT;
 
 const port = process.env.PORT || 3000
 
 app.get('/ping', async (_req, res) => {
-  //res.send('pong')
   const response = await fetch(`http://localhost:${daprPort}/v1.0/invoke/hr.hr/method/ping`)
   const body = await response.text()
   res.send("Respose from hr api: " + body)

e4bba540729973cb478e716b1a6c8725b35135fd
Cedd Burge <ceddlyburge@gmail.com>
2021-04-20 15:49:38 +0100

    Add a get manager endpoint to the slack api, which calls the hr api

--- lib/slack/src/index.ts
+++ lib/slack/src/index.ts
@@ -1,18 +1,49 @@
-import express from 'express'
+import express, { response } from 'express'
 import fetch from 'node-fetch';
 
 const app = express()
 
-// This environment variables are injected automatically into the container by dapr
+// This environment variable is injected automatically into the container by dapr
+// DAPR_GRPC_PORT is also injected, although we don't use it
 const daprPort = process.env.DAPR_HTTP_PORT;
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.get('/ping', async (_req, res) => {
   const response = await fetch(`http://localhost:${daprPort}/v1.0/invoke/hr.hr/method/ping`)
   const body = await response.text()
   res.send("Respose from hr api: " + body)
 
+})
+
+// This is designed as a Rest endpoint, although the HR api is GraphQL. I'm not sure which
+// is best for this, but presumably this slack api is a Backends for Frontends type Api, in
+// which case Rest, or Rpc is probably going to be more suitable that GraphQl.
+app.get('/:userId/manager', async (req, res) => {
+  // We will probably want to use relay or something later as the GraphQl usage gets more 
+  // complex, but I think hand rolling things is fine for now
+  const query =
+    `{
+        human(id: "${req.params.userId}") {
+          id
+          name
+          manager {
+            id
+            name
+          }
+        }
+      }`
+
+  fetch(`http://localhost:${daprPort}/v1.0/invoke/hr.hr/method/graphql`, {
+    "body": JSON.stringify(
+      {
+        "query": query
+      }),
+    "method": "POST",
+  })
+    .then(response => response.json())
+    .then(json => res.send("Respose from hr api: " + json.data.human.manager.name))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => console.log(`http://localhost:${port}`))

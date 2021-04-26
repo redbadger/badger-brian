97216d94fb345aa0b1af3df2ed1d8e4b8418b0f3
Cedd Burge <ceddlyburge@gmail.com>
2021-04-26 12:44:48 +0100

    Expose slack api on port 3001, so it doesn't clash with hr api
    This makes no difference in kubernetes, as everything gets an internal
    cluster ip address, but is useful when running locally outside of k8s

--- lib/slack/src/index.ts
+++ lib/slack/src/index.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.get('/hello', async (_req, res) => {
   res.send("world")

d3576206b653f98e79502e4f292a56ddc499a35a
Cedd Burge <ceddlyburge@gmail.com>
2021-04-29 10:41:23 +0100

    Use passed fetchManager, to fix violation of onion architecture

--- lib/slack/src/domain.ts
+++ lib/slack/src/domain.ts
@@ -1,10 +1,9 @@
 import { SlackReply, composeSlackReply as coreComposeSlackReply } from './core';
-import { fetchManager } from './infrastructure';
 
 
 async function composeSlackReply(
   userName: String,
-  getManager: (userName: String) => Promise<String>
+  fetchManager: (userName: String) => Promise<String>
 ): Promise<SlackReply> {
   const manager = await fetchManager(userName)
   return coreComposeSlackReply(userName, manager)
